6f1562c2078cf0b925592ab7973e32d538d1e869
jaspinder <jaspinder@ucreate.co.in>
2017-02-02 18:31:52 +0530

    sdsd

--- app.js
+++ app.js
@@ -3,32 +3,72 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
+var express = require('express') 
   , http = require('http')
-  , path = require('path');
+  , path = require('socket.io');
+   var io = require('socket.io')(require('http').Server(app));
 
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/hero/:name', routes.hero);
-app.post('/hero/add-fact', routes.addFact);
+ var userlist = [];
+
+    io.on('connection', function (socket) {
+   
+    if (socket.handshake.query.id) {
+      var found=false;
+      for(var i = 0; i < userlist.length; i++) {
+          if (userlist[i].socketid == socket.id) {
+              found = true;
+              break;
+          } 
+        }
+        if(!found){
+           console.log(socket.handshake.query.id);
+           var user={id: socket.handshake.query.id,socketid: socket.id};
+           userlist.push(user);
+        }       
+     }
+
+    console.log("client connected")
+     socket.on('pulllist', function () {
+        console.log('test');         
+        io.sockets.emit('getlist', userlist);
+      });
+
+      socket.on('sendmessage', function ( data) {
+       for(var i = 0; i < userlist.length; i++) {
+            if(userlist[i].socketid === data.receiver) {
+               console.log('message sent to' + userlist[i].socketid)
+               io.sockets.connected[userlist[i].socketid].emit('message', {  message: data.message, sender: data.sender, receiver: data.receiver});
+             break;
+            }
+         }      
+     });
+
+      socket.on('disconnect', function() {
+        //var index=userlist.findIndex(x => x.socketid == socket.id);
+         var id = '';
+         for(var i = 0; i < userlist.length; i += 1) {
+            if(userlist[i].socketid === socket.id) {
+              id = userlist[i].id;
+              userlist.splice(i, 1);
+            }
+         }
+           io.sockets.emit('disconnected', id);
+           if (socket.id !== undefined) {      
+              socket.leave(socket.id);
+           }
+      });
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
