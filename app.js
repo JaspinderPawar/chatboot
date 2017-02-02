ff1610dd6894346525049cc13b5c3544ac987fc2
jaspinder <jaspinder@ucreate.co.in>
2017-02-02 20:30:23 +0530

    zx

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.configure('development', function(){
 });
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:60455");
+  
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
