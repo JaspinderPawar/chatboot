8c8b2a0e0f46c7e7033f158fb8d6d2bd6569ac52
jaspinder <jaspinder@ucreate.co.in>
2017-02-02 20:18:42 +0530

    xc

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.configure('development', function(){
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:60455");
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
