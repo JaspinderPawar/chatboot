25227e748d48edd8966d434b774ff050fdfaf1c0
jaspinder <jaspinder@ucreate.co.in>
2017-02-07 18:19:41 +0530

    nbnbn

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ if (origins.toString().length > 0) {
 
 app.use(function(req, res, next) {  
   if(allowedOrigins.indexOf(origin) > -1){
-       res.setHeader('Access-Control-Allow-Origin', origin);
+       res.header('Access-Control-Allow-Origin', origin);
   }
   //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
